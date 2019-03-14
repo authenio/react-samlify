06afc6aec3953082e11b2a936510f65ea725d4ae
tngan <tonynwk919@gmail.com>
2019-03-14 18:58:55 +0800

    Add minimal UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from './Home';
+import { Helmet } from "react-helmet";
 
 export class App extends React.PureComponent {
   render() {
     return (
-      <Router>
-        <Route path="/" component={Home} />
-      </Router>
+      <React.Fragment>
+        <Helmet>
+          <title>react-samlify</title> 
+          <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
+        </Helmet>
+        <Router>
+          <Route path="/" component={Home} />
+        </Router>
+      </React.Fragment>
     );
   }
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,100 +1,118 @@
-import * as React from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { parse } from 'query-string';
 import axios from 'axios';
 
 const LOCALSTORAGE_TOKEN_FIELD = 'auth_token';
 
-type Props = RouteComponentProps & {
-
-};
+type Props = RouteComponentProps & {};
 
 type Profile = {
   email: string;
 }
 
-type State = {
-  authenticated: boolean;
-  profile: Profile;
-}
+const Container = (props: { children: ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center vh-100 system-sans-serif"> 
+      {props.children}
+    </div>
+  )
+};
 
-export class Home extends React.Component<Props, State> {
+const Button = (props: { children: ReactNode; onClick: Function; }) => {
+  return (
+    <button
+      style={{ border: '1px solid #aaa' }}
+      className="pa3 bg-transparent ma2 br3 f6 silver-gray outline-0 pointer"
+      onClick={() => props.onClick()}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      authenticated: false,
-      profile: null
-    };
-  }
+export function Home(props: Props) {
 
-  _initRedirectRequest() {
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const [profile, setProfile] = useState<Profile>({ email: null });
+
+  const initRedirectRequest = () => {
     window.location.href = '/sso/redirect';
-  }
+  };
 
-  _initPostRequest() {
+  const initPostRequest = () => {
     window.location.href = '/sso/post';
-  }
+  };
 
-  _viewMetadata() {
+  const viewMetadata = () => {
     window.location.href = '/metadata';
-  }
+  };
 
-  _logout() {
+  const logout = () => {
     window.localStorage.removeItem(LOCALSTORAGE_TOKEN_FIELD);
-    this.setState({ authenticated: false, profile: null });
+    setAuthenticated(false);
+    setProfile({ email: null });
   }
 
-  async getProfile(token) {
+  const getProfile = async (token: string) => {
     try {
       const { data } = await axios.get<{profile: Profile}>('/profile', { headers: { Authorization: `Bearer ${token}` } });
-      this.setState({ authenticated: true, profile: data.profile });
+      setAuthenticated(true);
+      setProfile(data.profile);
     } catch (e) {
-      this.setState({ authenticated: false, profile: null })
+      setAuthenticated(false);
+      setProfile({ email: null });
       window.localStorage.removeItem(LOCALSTORAGE_TOKEN_FIELD);
     }
-  }
+  };
 
-  componentWillMount() {
-    this.init();
-  }
-
-  async init() {
+  const init = async () => {
     const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_FIELD);
     // if the token is already stored in localstoarge, call the service to verify if it's expired
     // if anything wrong, go back to the login scene
     if (token) {
       // verify the current auth token
-      return await this.getProfile(token);
+      return await getProfile(token);
     }
     // this section
-    const params = parse(this.props.location.search);
+    const params = parse(props.location.search);
     if (params.auth_token) {
       window.localStorage.setItem(LOCALSTORAGE_TOKEN_FIELD, params.auth_token);
-      await this.getProfile(params.auth_token);
+      await getProfile(params.auth_token);
       // remove the auth_token part in 
-      this.props.history.replace('/');
+      props.history.replace('/');
     }
     // initial state
-  }
+  };
 
-  render() {
-    if (!this.state.authenticated) {
-      return (
-        <React.Fragment>
-          <button onClick={() => this._initRedirectRequest()}>Okta SSO Login - redirect</button>
-          <button onClick={() => this._initPostRequest()}>Okta SSO Login - post</button>
-          <button onClick={() => this._viewMetadata()}>Metadata</button>
-        </React.Fragment>
-      );
-    }
-    {
-      /** render screen after login in */
-    }
-    return <>
-      Welcome <b>{this.state.profile.email}</b>
-      <button onClick={() => this._logout()}>Logout</button>
-    </>
+  useEffect(() => {
+    init();
+    return () => null;
+  }, [])
+
+  if (!authenticated) {
+    return (
+      <Container>
+        <Button onClick={() => initRedirectRequest()}>
+          Okta - redirect
+        </Button>
+        <Button onClick={() => initPostRequest()}>
+          Okta - post
+        </Button>
+        <Button onClick={() => viewMetadata()}>
+          Metadata
+        </Button>
+      </Container>
+    );
   }
+  {
+    /** render screen after login in */
+  }
+  return <Container>
+    <div className="flex flex-column">
+      <span className="mb3">Welcome back <b>{profile.email}</b></span>
+      <Button onClick={() => logout()}>Logout</Button>
+    </div>
+  </Container>
 
 }
